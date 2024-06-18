0a557327a57fabd8e82f7073cf7db06b88a9dd20
Muhammad Bilal <150532599+laptopgithubit@users.noreply.github.com>
2024-06-18 23:39:57 +0500

    Add files via upload

--- checkbox/checkbox.py
+++ checkbox/checkbox.py
@@ -1,1 +1,25 @@
+import tkinter as tk
 
+def display1():
+    print("Checkbox of x " , x.get())
+
+def display2():
+    print("Checkbox of y " , y.get())
+
+win = tk.Tk()
+win.title("My First Application")
+win.minsize(500,500)
+
+x = tk.IntVar()
+y = tk.IntVar()
+ch1 = tk.Checkbutton(win, text="C++", variable=x, command=display1)
+ch2 = tk.Checkbutton(win, text="Java", variable=y, command=display2)
+x.set(0)
+y.set(0)
+
+ch1.pack()
+ch2.pack()
+
+
+
+win.mainloop()
